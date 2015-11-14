3db4f556f13180fb93bac1aa55b60f3b93e11d0a
Val Packett <val@packett.cool>
2015-11-14 23:06:18 +0300

    add more import

--- web-app.go
+++ web-app.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"strings"
 
+	now "github.com/jinzhu/now"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -21,6 +22,7 @@ func main() {
 	var port = os.Getenv("PORT")
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./settings-app/"))))
 	http.HandleFunc("/import/myclassschedule", mcsImportHandler)
+	http.HandleFunc("/import/ruz", ruzImportHandler)
 	fmt.Println("Server started on port " + port)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
@@ -37,6 +39,18 @@ type Class struct {
 type Day struct {
 	Day      string  `json:"day"`
 	Schedule []Class `json:"schedule"`
+}
+
+func respondWithSchedule(res http.ResponseWriter, req *http.Request, days []Day) {
+	jsonBytes, _ := json.Marshal(days)
+	var scheme string
+	if strings.Contains(req.Host, "localhost") {
+		scheme = "http"
+	} else {
+		scheme = "https"
+	}
+	res.Header().Set("Location", fmt.Sprintf("%s://%s/classyclock/static/settings.html#%s", scheme, req.Host, url.QueryEscape(string(jsonBytes))))
+	res.WriteHeader(http.StatusFound)
 }
 
 func mcsImportHandler(res http.ResponseWriter, req *http.Request) {
@@ -89,13 +103,64 @@ func mcsImportHandler(res http.ResponseWriter, req *http.Request) {
 		}
 	}
 	rows.Close()
-	jsonBytes, _ := json.Marshal([]Day{mon, tue, wed, thu, fri, sat, sun})
-	var scheme string
-	if strings.Contains(req.Host, "localhost") {
-		scheme = "http"
-	} else {
-		scheme = "https"
+	respondWithSchedule(res, req, []Day{mon, tue, wed, thu, fri, sat, sun})
+}
+
+type RuzClass struct {
+	Room  string `json:"auditorium"`
+	Subj  string `json:"discipline"`
+	Start string `json:"beginLesson"`
+	End   string `json:"endLesson"`
+	Day   int    `json:"dayOfWeek"`
+}
+
+func ruzImportHandler(res http.ResponseWriter, req *http.Request) {
+	now.FirstDayMonday = true
+	query := req.URL.Query()
+	params := url.Values{}
+	params.Add("fromdate", now.BeginningOfWeek().Format("2006.01.02"))
+	params.Add("todate", now.EndOfWeek().Format("2006.01.02"))
+	params.Add("email", query.Get("email"))
+	ruzreq, err := http.NewRequest("GET", "http://ruz.hse.ru/RUZService.svc/personlessons?"+params.Encode(), nil)
+	ruzreq.Header.Add("User-Agent", "Mozilla/5.0 (Linux; Android 5.1.1; One Build/LRX22C.H3) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36")
+	ruzreq.Header.Add("Accept", "application/json, text/plain, */*")
+	ruzreq.Header.Add("X-Requested-With", "ru.hse.ruz")
+	client := &http.Client{}
+	ruzres, err := client.Do(ruzreq)
+	defer ruzres.Body.Close()
+	if err != nil {
+		panic(err)
 	}
-	res.Header().Set("Location", fmt.Sprintf("%s://%s/classyclock/static/settings.html#%s", scheme, req.Host, url.QueryEscape(string(jsonBytes))))
-	res.WriteHeader(http.StatusFound)
+	var rclasses []RuzClass
+	err = json.NewDecoder(ruzres.Body).Decode(&rclasses)
+	if err != nil {
+		panic(err)
+	}
+	mon := Day{"Monday", make([]Class, 0)}
+	tue := Day{"Tuesday", make([]Class, 0)}
+	wed := Day{"Wednesday", make([]Class, 0)}
+	thu := Day{"Thursday", make([]Class, 0)}
+	fri := Day{"Friday", make([]Class, 0)}
+	sat := Day{"Saturday", make([]Class, 0)}
+	sun := Day{"Sunday", make([]Class, 0)}
+	for _, rclass := range rclasses {
+		class := Class{rclass.Start, rclass.End, rclass.Room + " " + rclass.Subj}
+		switch rclass.Day {
+		case 1:
+			mon.Schedule = append(mon.Schedule, class)
+		case 2:
+			tue.Schedule = append(tue.Schedule, class)
+		case 3:
+			wed.Schedule = append(wed.Schedule, class)
+		case 4:
+			thu.Schedule = append(thu.Schedule, class)
+		case 5:
+			fri.Schedule = append(fri.Schedule, class)
+		case 6:
+			sat.Schedule = append(sat.Schedule, class)
+		case 7:
+			sun.Schedule = append(sun.Schedule, class)
+		}
+	}
+	respondWithSchedule(res, req, []Day{mon, tue, wed, thu, fri, sat, sun})
 }
